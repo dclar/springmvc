928bab6ef211a722ded906e302e747a9821ff18b
dclar <forevervip.lu@gmail.com>
2018-02-12 17:30:32 +0800

    编写udp协议发送请求测试flume的syslogudp的source

---
+++ flume/src/main/java/org/dclar/flume/showcase/sendudp/TestSendUdp.java
@@ -0,0 +1,27 @@
+package org.dclar.flume.showcase.sendudp;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+
+public class TestSendUdp {
+
+
+    @Test
+    public void send() throws IOException {
+
+        DatagramSocket socket = new DatagramSocket();
+        byte[] data = "hello world, from mac".getBytes();
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+
+        packet.setAddress(Inet4Address.getByName("centos01"));
+        packet.setPort(8888);
+        socket.send(packet);
+        socket.close();
+
+    }
+}
